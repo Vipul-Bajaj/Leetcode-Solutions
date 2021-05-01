91c2ce613ffe8d14740b8f6394eb30fcb3d1da3e
Vipul Bajaj <vipulbjj@gmail.com>
2021-05-01 21:11:03 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import os, urllib.parse
 files_to_ignore = ["README.md"]
-folders_to_ignore = [".github",".git"]
+folders_to_ignore = [".github"]
 root_folder = "."
 root_readme = open(os.path.join(root_folder,"README.md"),"w+")
 root_readme.write("# Index\n")
@@ -9,16 +9,17 @@ for company_folder in sorted(os.listdir(root_folder)):
     if os.path.isdir(os.path.join(root_folder, company_folder)) and company_folder not in folders_to_ignore:
         comapany_level = os.path.join(root_folder, company_folder)
         company_readme = open(os.path.join(comapany_level,"README.md"),"w+")
-        company_readme.write("# Index\n")
+        company_readme.write("# [Leetcode-Solutions](./..)/" + "[" + company_folder + "](./)\n")
         difficulty_idx = 1
         for difficulty_folder in os.listdir(comapany_level):
             if os.path.isdir(os.path.join(company_folder,difficulty_folder)):
                 difficulty_level = os.path.join(company_folder,difficulty_folder)
                 difficulty_readme = open(os.path.join(difficulty_level,"README.md"),"w+")
-                difficulty_readme.write("# Index\n")
+                difficulty_readme.write("# [Leetcode-Solutions](./../..)/[" + company_folder + "](./..)/[" + difficulty_folder +"](./)\n")
                 file_idx = 1
                 for files in sorted(os.listdir(difficulty_level)):
                     if files not in files_to_ignore:
+                        os.rename(os.path.join(difficulty_level,files), os.path.join(difficulty_level,files.replace(" ", "_")))
                         file_name,file_extension = os.path.splitext(files)
                         difficulty_readme.write(str(file_idx) + ". [" + file_name + "](" + urllib.parse.quote(os.path.join('.',files)) + ")\n")
                         file_idx +=1
